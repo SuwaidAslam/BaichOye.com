35e6f7b3585f976d75df6822f95544353dd6664e
Suwaid <suwaidaslam@gmail.com>
2023-07-13 17:59:07 +0500

    made changes

--- admin/src/constants/url.js
+++ admin/src/constants/url.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'https://baichoye-server-0e312758a541.herokuapp.com/';
+export const SERVER_URL = 'http://localhost:5000/';
 export const STATIC_FILES_URL = SERVER_URL + 'resources/';
